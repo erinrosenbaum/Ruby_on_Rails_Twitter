305d37eb36a5364a5815565fe8089b92beb8dcd8
erinrosenbaum <erinrosenbaum@yahoo.com>
2016-12-27 21:02:12 -0600

    Added account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Application!"
-      redirect_to @user
+      # Created send_activation_email method (in user model) in 11.35 
+      @user.send_activation_email 
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
